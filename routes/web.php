9f8ff983c4a9b97ceb51df6ddb2375dbfefd5c25
Joaquimlp <joaquimlp2013@gmail.com>
2022-02-27 19:37:02 -0300

    Create controller de usuário

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -13,6 +14,8 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+Route::get("/user", [UserController::class, 'index'])->name("user.index");
+Route::get("/user/{id}", [UserController::class, 'show'])->name("user.show");
 Route::get('/', function () {
     return view('welcome');
 });
